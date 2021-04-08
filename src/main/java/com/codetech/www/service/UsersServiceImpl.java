4c7434ee1ee9e6ab98e07edde01573af661758dd
Adrian <akabongee@gmail.com>
2021-04-08 10:36:32 +0900

    code reformat after merge

--- src/main/java/com/codetech/www/service/UsersServiceImpl.java
+++ src/main/java/com/codetech/www/service/UsersServiceImpl.java
@@ -12,35 +12,34 @@ import com.codetech.www.domain.User;
 import com.codetech.www.domain.UserInfo;
 
 
-
 @Service
-public class UsersServiceImpl implements UsersService{
-	private static final Logger logger = LoggerFactory.getLogger(UsersServiceImpl.class);
-	
-	@Autowired
-	private UsersDAO dao;
-	
-  @Override
-	public int getSearchListCount(int index, int state, String search_word) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+public class UsersServiceImpl implements UsersService {
+    private static final Logger logger = LoggerFactory.getLogger(UsersServiceImpl.class);
 
-	@Override
-	public int getNoticeListCount() {
-		return dao.getNoticeListCount();
-	}
-  
-  
-	@Override
-	public int isEmail(String user_email) {
-		User user = dao.isEmail(user_email);
-		return (user == null) ? -1 : 1;
-	}
+    @Autowired
+    private UsersDAO dao;
 
-	@Override
-	public int isName(String user_name) {
-		UserInfo user = dao.isName(user_name);
-		return (user == null) ? -1 : 1;
-	}
+    @Override
+    public int getSearchListCount(int index, int state, String search_word) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNoticeListCount() {
+        return dao.getNoticeListCount();
+    }
+
+
+    @Override
+    public int isEmail(String user_email) {
+        User user = dao.isEmail(user_email);
+        return (user == null) ? -1 : 1;
+    }
+
+    @Override
+    public int isName(String user_name) {
+        UserInfo user = dao.isName(user_name);
+        return (user == null) ? -1 : 1;
+    }
 }
