a6c01a59e0c023acbc00c9834322a28888fdf096
unknown <altn0729.naver.com>
2021-04-13 22:32:24 +0900

    pis 21-04-13

--- src/main/resources/sql/codeTheh.sql
+++ src/main/resources/sql/codeTheh.sql
@@ -1,4 +1,4 @@
-drop table role cascade constraints;
+drop table role cascade constraints ;
 create table role
 (
     role_id    number(1) primary key,
@@ -6,15 +6,15 @@ create table role
 );
 
 insert into role
-values (1, 'site_owner');
+values (1, 'ROLE_SITE_OWNER');
 insert into role
-values (2, 'site_admin');
+values (2, 'ROLE_ADMIN');
 insert into role
-values (3, 'store_owner');
+values (3, 'ROLE_STORE_OWNER');
 insert into role
-values (4, 'store_staff');
+values (4, 'ROLE_STORE_STAFF');
 insert into role
-values (5, 'user');
+values (5, 'ROLE_MEMBER');
 
 drop table user_status cascade constraints;
 
