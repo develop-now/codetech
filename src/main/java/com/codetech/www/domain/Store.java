918c99a136ab88fc3913524ce46e15fa5d298479
Adrian <akabongee@gmail.com>
2021-04-13 23:17:00 +0900

    store.sql and store.xml edit

--- src/main/java/com/codetech/www/domain/Store.java
+++ src/main/java/com/codetech/www/domain/Store.java
@@ -23,50 +23,15 @@ public class Store {
     private int owner_id;
     private int store_status;
 
-    private int like_count;
-    private int comment_count;
-
-
-
-    public int getLike_count() {
-		return like_count;
-	}
-
-	public void setLike_count(int like_count) {
-		this.like_count = like_count;
-	}
-
-	public int getComment_count() {
-		return comment_count;
-	}
-
-	public void setComment_count(int comment_count) {
-		this.comment_count = comment_count;
-	}
-
-	private MultipartFile store_image;
+    private MultipartFile store_image;
 
     private String store_saved_image; // 실제 저정된 파일 이름
     private String store_original_image; // 첨부될 파일 이름
 
     private String store_status_value;
 
-
-    public int getStore_like() {
-        return store_like;
-    }
-
-    public void setStore_like(int store_like) {
-        this.store_like = store_like;
-    }
-
-    public int getStore_comment() {
-        return store_comment;
-    }
-
-    public void setStore_comment(int store_comment) {
-        this.store_comment = store_comment;
-    }
+    private int like_count;
+    private int comment_count;
 
     public int getStore_id() {
         return store_id;
@@ -250,5 +215,21 @@ public class Store {
 
     public void setStore_status_value(String store_status_value) {
         this.store_status_value = store_status_value;
+    }
+
+    public int getLike_count() {
+        return like_count;
+    }
+
+    public void setLike_count(int like_count) {
+        this.like_count = like_count;
+    }
+
+    public int getComment_count() {
+        return comment_count;
+    }
+
+    public void setComment_count(int comment_count) {
+        this.comment_count = comment_count;
     }
 }
