a7c45ec372f958d72c2249c97083c93464b258a5
Adrian <akabongee@gmail.com>
2021-04-26 18:30:30 +0900

    edit order count socket

--- src/main/java/com/codetech/www/task/OrderCountHandler.java
+++ src/main/java/com/codetech/www/task/OrderCountHandler.java
@@ -13,11 +13,19 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class OrderCountHandler extends TextWebSocketHandler {
     private static final Logger logger = LoggerFactory.getLogger(OrderCountHandler.class);
+
+
+    List<WebSocketSession> sessions = new ArrayList<WebSocketSession>();
+    Map<String, WebSocketSession> users = new ConcurrentHashMap<String, WebSocketSession>();
+
 
     @Autowired
     private StoreService storeService;
@@ -25,35 +33,62 @@ public class OrderCountHandler extends TextWebSocketHandler {
     @Autowired
     private OrderService orderService;
 
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        logger.error(session.getId() + " 에러 발생: " + exception.getMessage());
+    }
+
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        String senderId = getMemberId(session);
+        if (senderId != null) { // 로그인 값이 있는 경우만
+            logger.info(senderId + " 연결 종료됨");
+            users.remove(senderId);
+            sessions.remove(session);
+        }
+    }
 
+    private String getMemberId(WebSocketSession session) {
+        Map<String, Object> httpSession = session.getAttributes();
+        return (String) httpSession.get("user_idS");
     }
 
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-
+        String senderId = getMemberId(session); // 접속한 유저의 http세션을 조회하여 id를 얻는 함수
+        if (senderId != null) { // 로그인 값이 있는 경우만
+            logger.info(senderId + " 연결 됨");
+            users.put(senderId, session); // 로그인중 개별유저 저장
+        }
     }
 
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String senderId = getMemberId(session);
+        String target = message.getPayload();
+        logger.info("socket target: "+ target);
+        WebSocketSession targetSession = users.get(target);
+        logger.info("socket targetSession: "+ targetSession);
+
         logger.info("[OrderCountHandler] STORE OWNER ID : " + message.getPayload());
 
         int owner_id = Integer.parseInt(message.getPayload());
         List<Store> stores = storeService.getStoreListByOwner(owner_id);
 
-        if (stores.size() > 0) {
-            int store_total_order = 0;
+        int store_total_order = 0;
 
+        if (stores.size() > 0 && targetSession != null) {
             for (Store s : stores) {
                 store_total_order += orderService.getNewOrderCountByStore(s.getStore_id());
             }
 
-            session.sendMessage(new TextMessage(Integer.toString(store_total_order)));
+            TextMessage tmpMsg = new TextMessage(Integer.toString(store_total_order));
+            targetSession.sendMessage(tmpMsg);
             logger.info("## ORDER COUNT MESSAGE SEND!");
         }
+
     }
 }
--- src/main/webapp/WEB-INF/views/owner/paySuccess.jsp
+++ src/main/webapp/WEB-INF/views/owner/paySuccess.jsp
@@ -2,40 +2,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link
-            href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-            rel="stylesheet" id="bootstrap-css">
-    <script
-            src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script
-            src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
     <meta charset='UTF-8'>
 
     <title>Pay Success</title>
     <%@include file="../partial/head.jsp" %>
-
-
 </head>
 <body>
 <%@include file="../partial/nav.jsp" %>
 <div class="container">
     <div class="dark">
         <input type="hidden" class="amount" name="amount" value="${amount }"
-               id="amount"> <input type="hidden" id="url"
-                                   value="${pageContext.request.contextPath}"> <input
+               id="amount"> <input type="hidden" id="url" value="${pageContext.request.contextPath}"> <input
             type="hidden" name="ordered_store" id="ordered_store"
             class="ordered_store" value="${orderedStore}">
-        <input
-                type="hidden" name="store_owner" id="store_owner"
-                class="store_owner" value="${store_owner}">
-
+        <input type="hidden" name="store_owner" id="store_owner"
+               class="store_owner" value="${store_owner}">
     </div>
     <br><br>
     <div class="img" style="text-align:center; ">
         <img class="success"
              src="${pageContext.request.contextPath}/resources/image/common/success.png"
-             width="500" height="500" alt="">
+             width="500" height="500" alt="success_img">
     </div>
     <br><br>
     <div style="text-align:center;">
@@ -46,9 +33,6 @@
     </div>
     <script>
         $(document).ready(function () {
-
-            console.log("success page loaded!")
-
             socket.onopen = function () {
                 var target = $('#store_owner').val();
                 var content = '주문이 접수되었습니다.';
@@ -56,7 +40,9 @@
                 socket.send("관리자," + target + "," + content + "," + url); // 소켓에 전달
             }
 
-            sendOrderNotification($('#store_owner').val());
+            order_count_ws.onopen = function () {
+                order_count_ws.send($('#store_owner').val())
+            }
         });
     </script>
 </div>
